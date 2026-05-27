286059012b59bad496ce9724552d215dfeddccb0
Divyansh Gupta <gdivyansh442@gmail.com>
2026-05-28 00:56:36 +0530

    que: best time to buy a stock

---
+++ Array/bestTimeStock.js
@@ -0,0 +1,17 @@
+function timeToBuy (stockPrices) {
+    let todaysProfit = 0;
+    let maxProfit = 0;
+    
+    let minimumPrice = stockPrices[0]
+    for(let i= 0; i< stockPrices.length; i++) {
+        if (stockPrices[i] < minimumPrice) {
+            minimumPrice = stockPrices[i]
+        }
+        todaysProfit = stockPrices[i] - minimumPrice;
+        if (todaysProfit > maxProfit) {
+            maxProfit = todaysProfit;
+        }
+    }
+    return maxProfit
+}
+console.log(timeToBuy([4, 1, 2, 1]))
--- Array/twoSum.js
+++ Array/twoSum.js
@@ -1,73 +1,30 @@
-// const arr2 = [2, 7, 11, 15, 3, 6, 0, 4, 6, 3, 9, 10]
-// const target2 = 9
-
-
-// for (let i = 0; i < arr2.length -1 ; i++) {
-//     for(let j = i + 1; j < arr2.length; j++) {
-//         if ((arr2[i] + arr2[j]) === target2) {
-//             console.log(i, j)
-//         }
-//     }
-// }
-
-// HASH MAP
-
-const arr2 = [2, 7, 11, 15, 3, 6, 0, 4, 6, 3, 9, 10]
-const target2 = 9
-
-function twoSum(array, target) {
-    const map = new Map();
-
-for (let i = 0; i < arr2.length; i++) {
-    const num = arr2[i]
-    const needed = target2 - num
-    if (map.has(needed)) {
-        console.log(map.get(needed), i)
+function twoSum (array, target) {
+    for (let i = 0; i < array.length; i++) {
+        for(let j = i + 1; j < array.length; j++) {
+            if ((array[i] + array[j]) === target) {
+                console.log(i, j)
+            }
+        }
     }
-    map.set(num, i);
 }
-return null;
-}
-twoSum(arr2, target2)
+twoSum([2, 7, 11, 15, 3, 6, 0, 4, 6, 3, 9, 10], 9)
 
+//HASH MAP
+// function twoSum(array, target) {
+//     const map = new Map();
 
-// const arr = [2, 7, 11, 15, 3, 6, 0, 4, 6, 3, 9, 10]
-// const target = 9
-// const set = new Set();
-
-// for (let i = 0; i < arr.length; i++) {
-//     const needed = target - arr[i];
-
-//     if (set.has(needed)) {
-//         console.log(i)
-//     }
-//     set.add(arr[i])
-// }
-
-// const arr = [1, 2, 3, 4, 5]
-// const target = 5
-// let count = 0;
-// for (let i = 0; i<arr.length-1; i++){
-//     for(let j = i + 1; j< arr.length; j++) {
-//         if ((arr[i] + arr[j]) === target) {
-//             count++;
+//     for (let i=0; i< array.length; i++) {
+//         const needed = target - array[i]
+//         if (map.has(needed)) {
+//             return [map.get(needed), i];
 //         }
+//         map.set(array[i], i);
+        
 //     }
 // }
-// console.log(count)
+// twoSum([2, 7, 11, 15, 3, 6, 0, 4, 6, 3, 9, 10], 19)
 
-// const arr = [1, 2, 3, 4, 5]
-// const target = 5
-// let count = 0;
 
-// const set = new Set();
 
-// for (let num of arr) {
-//     const needed = target - num;
 
-//     if( set.has(needed)) {
-//         count++;
-//     }
-//     set.add(num);
-// }
-// console.log(count)
+
